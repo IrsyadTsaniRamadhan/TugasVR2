2409e258ba91ab3731112bc35682042398315314
IrsyadTsaniRamadhan <42790631+IrsyadTsaniRamadhan@users.noreply.github.com>
2020-09-15 16:43:13 +0800

    Tugas VR 2
    Pengumpulan Tugas 2

---
+++ Assets/UbahWarnaCylinder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UbahWarnaCylinder : MonoBehaviour
+{
+   public void Cyan(){
+        GetComponent<Renderer>().material.color = Color.cyan;
+    }
+    public void Gray(){
+        GetComponent<Renderer>().material.color = Color.gray;
+    }
+    public void Magenta(){
+        GetComponent<Renderer>().material.color = Color.magenta;
+    }
+}
---
+++ Assets/UbahWarnaSphere.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UbahWarnaSphere : MonoBehaviour
+{
+   public void Putih(){
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+    public void kuning(){
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+    public void Hijau(){
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+}
